97d7c96503068518d3e32273a2414c79ae2f598b
dollyf <dollyf@users.noreply.github.com>
2015-08-07 11:23:36 -0700

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,7 +1,7 @@
 # Read the power comsumption text file and load it to a dataframe
 plotdata <- read.table("./data/household_power_consumption.txt", sep =";", header=TRUE)
 
-# Extract only data for Feb 1st and 2nd
+# Extract only data for Feb 1st and 2nd of Feb 2007
 febplotdata <- subset(plotdata, grepl("^(1/2/2007|2/2/2007)", plotdata$Date))
 
 # Generate a histogram for the Global active power and save it as plot1.png
